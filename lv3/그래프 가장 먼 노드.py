5b315005c3f05553b4d93944400d30d1d00f8f8f
oguuk <ogw135@gmail.com>
2022-03-04 23:47:11 +0900

    3/4

--- lv3/그래프 가장 먼 노드.py
+++ lv3/그래프 가장 먼 노드.py
@@ -22,3 +22,37 @@ def dfs(vertex,cost):
         if fee[i[0]] < cost+1:
             continue
         dfs(i,cost+1)
+
+
+#3/4
+
+from collections import deque, Counter
+
+def solution(n, edge):
+    answer = 0
+    graph = {}
+    for i in edge:
+        if i[0] in graph:
+            graph[i[0]].append(i[1])
+        else:
+            graph[i[0]] = [i[1]]
+        if i[1] in graph:
+            graph[i[1]].append(i[0])
+        else:
+            graph[i[1]] = [i[0]]
+    
+    result = bfs(graph,1)
+    result = sorted(result.values(), reverse=True)
+    return list(Counter(result).values())[0]
+
+def bfs(edge,root):
+    visited = {}
+    queue=deque([[root,0]])
+    arr = [[root,0]]
+    while queue:
+        current = queue.popleft()
+        if current[0] not in visited:
+            visited[current[0]] = current[1]
+            add = set(edge[current[0]]) - set(visited)
+            queue += ([[i,current[1]+1] for i in add])
+    return visited
