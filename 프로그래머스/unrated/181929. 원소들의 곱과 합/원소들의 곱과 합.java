7fb0b15f6c54949ba1ad913e8de9e713ae812780
Gukwon Oh <ogw135@gmail.com>
2023-07-18 16:13:20 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 1.04 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,6 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] num_list) {
+        return Arrays.stream(num_list).reduce(1, (a, b) -> a * b) > Math.pow(Arrays.stream(num_list).sum(), 2) ? 0 : 1;
+    }
+}
