0cc0ab5833c6055cb90e86cad09846921fa2f493
Gukwon Oh <ogw135@gmail.com>
2023-07-19 13:51:00 +0900

    [unrated] Title: 접미사 배열, Time: 2.99 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,11 @@
+import java.util.stream.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        return IntStream.range(0, my_string.length())
+                .mapToObj(i -> my_string.substring(i))
+                .sorted()
+                .toArray(String[]::new);
+    }
+}
