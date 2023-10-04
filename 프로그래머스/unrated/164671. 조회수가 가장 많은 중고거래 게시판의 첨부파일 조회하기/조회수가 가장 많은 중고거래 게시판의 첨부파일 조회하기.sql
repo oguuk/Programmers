9384aae8e2031b6246032820c249da538a4aeb18
Gukwon Oh <ogw135@gmail.com>
2023-10-04 15:59:07 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,5 +1,5 @@
-select concat('/home/grep/src/', file.board_id, '/',file.file_id, file.file_name, file.file_ext) file_path
-from used_goods_file file
-join used_goods_board board on file.board_id = board.board_id
-where board.views = (select max(views) from used_goods_board)
-order by file.file_id desc
+select concat('/home/grep/src/', b.board_id, '/', f.file_id, f.file_name, f.file_ext) file_path
+from used_goods_board b
+left join used_goods_file f on b.board_id = f.board_id
+where b.views in (select max(views) from USED_GOODS_BOARD)
+order by f.file_id desc
