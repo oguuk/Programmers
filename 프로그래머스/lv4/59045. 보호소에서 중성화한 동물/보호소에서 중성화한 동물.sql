c545d0416bd906363f5a93fa4158c7ab3efd48f3
Gukwon Oh <ogw135@gmail.com>
2023-10-06 00:41:32 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,4 +1,6 @@
-SELECT i.animal_id, i.animal_type, i.name
+select o.animal_id, o.ANIMAL_TYPE, o.NAME
 from animal_ins i
-join animal_outs o on i.animal_id = o.animal_id
-where i.SEX_UPON_INTAKE like 'Intact%' and (o.SEX_UPON_OUTCOME like 'Spayed%' or o.SEX_UPON_OUTCOME like 'Neutered%')
+left join animal_outs o on i.animal_id = o.animal_id
+where i.SEX_UPON_INTAKE like 'Intact%' 
+and (o.SEX_UPON_OUTCOME like 'Neutered Male' or o.SEX_UPON_OUTCOME like 'Spayed Female') 
+order by animal_id
