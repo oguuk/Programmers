55f18aacc60aad3c4bd64599c1f2d507ffb50515
Gukwon Oh <ogw135@gmail.com>
2023-10-06 01:21:55 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,11 +1,11 @@
-SELECT cart_id
-from cart_products
-where cart_id in (select cart_id 
-                  from cart_products
-                  where name like 'Milk'
-                 ) and cart_id in (select cart_id
-                                   from cart_products
-                                   where name like 'Yogurt'
-                                  )
-group by cart_id
+select a.cart_id
+from (select id, cart_id, name, price
+      from cart_products
+      where name like 'Milk'
+     ) a
+left join (select id, cart_id, name, price
+           from cart_products
+           where name like 'Yogurt'
+          )  b on a.cart_id = b.cart_id
+where b.name is not null
 order by cart_id
