83d6eab8f67f251a5f05ad84e1b0c48551babdad
Gukwon Oh <ogw135@gmail.com>
2023-10-06 12:07:08 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,16 +1,24 @@
-with total_people as (
-    select count(user_id) count
-    from user_info
-    where joined like '2021%'
-),
-buy as (
-    select YEAR(o.SALES_DATE) YEAR, MONTH(o.SALES_DATE) MONTH, count(distinct o.user_id) PUCHASED_USERS
-    from ONLINE_SALE o
-    join USER_INFO u on u.user_id = o.user_id
-    where u.joined like '2021%'
+with INFO as (
+    select YEAR(sales_date) YEAR,
+    MONTH(sales_date) MONTH,
+    count(distinct user_id) count,
+    (select count(user_id) from user_info where joined like '2021%') user_sum
+    from online_sale
+    where user_id in (select user_id
+                        from online_sale
+                        where user_id in (select user_id
+                                          from user_info
+                                          where joined like '2021%'
+                                         )
+                       )
     group by YEAR, MONTH
 )
 
-select b.year, b.month, b.PUCHASED_USERS, round(b.PUCHASED_USERS / p.count, 1) PUCHASED_RATIO
-from buy b, total_people p
-order by year, month
+select YEAR(o.sales_date) YEAR,
+MONTH(o.sales_date) MONTH,
+i.count PUCHASED_USERS,
+ROUND(i.count / i.user_sum, 1) PUNCHASED_RATIO
+from online_sale o
+join INFO i on YEAR(o.sales_date) = i.year and MONTH(o.sales_date) = i.month
+group by YEAR, MONTH
+order by YEAR, MONTH
