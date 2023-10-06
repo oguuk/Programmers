7f1ab70aa09c9051c29e21651a7d8afd52a4573e
Gukwon Oh <ogw135@gmail.com>
2023-10-06 12:30:56 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,13 +1,13 @@
-select a.APNT_NO, a.PT_NAME, a.PT_NO, b.MCDP_CD, b.DR_NAME, a.APNT_YMD
-from (select a.APNT_NO, p.PT_NAME, a.PT_NO, a.APNT_YMD
-      from APPOINTMENT a
-      left join PATIENT p on a.PT_NO = p.PT_NO
-      where isnull(a.APNT_CNCL_YMD)
-     ) a
-left join (select a.APNT_NO, a.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
-           from APPOINTMENT a
-           left join DOCTOR d on a.MDDR_ID = d.DR_ID
-           where isnull(a.APNT_CNCL_YMD)
-          ) b on a.APNT_YMD = b.APNT_YMD and a.APNT_NO = b.APNT_NO
-where MCDP_CD = 'CS' and a.APNT_YMD like '2022-04-13%'
+with A as (
+    select a.apnt_no, p.pt_name, a.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
+    from appointment a
+    join patient p on a.pt_no = p.pt_no
+    join doctor d on a.mddr_id = d.dr_id and a.mcdp_cd = d.mcdp_cd
+    where a.apnt_ymd like '2022-04-13%' 
+    and a.mcdp_cd like 'CS' 
+    and a.APNT_CNCL_YN = 'N'
+)
+
+select apnt_no, pt_name, pt_no, mcdp_cd, dr_name, apnt_ymd
+from A
 order by APNT_YMD
