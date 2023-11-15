0459f816594131984ce593e855cd2a03d86933be
Gukwon Oh <ogw135@gmail.com>
2023-11-15 23:06:01 +0900

    [Silver III] Title: 파일 정리, Time: 464 ms, Memory: 79168 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.swift
+++ 백준/Silver/20291. 파일 정리/파일 정리.swift
@@ -1,10 +1,10 @@
 let n: Int = Int(readLine()!)!
-var dict: [String : [String]] = [:]
+var dict: [String : Int] = [:]
 
 for _ in 0..<n {
     let file: [String] = readLine()!.split(separator: ".").map(String.init)
-    if dict[file[1]] == nil { dict[file[1]] = [] }
-    dict[file[1]]?.append(file[0])
+    if dict[file[1]] == nil { dict[file[1]] = 0 }
+    dict[file[1]]! += 1
 }
 
-dict.keys.sorted().forEach { print($0, dict[$0]!.count) }
+dict.keys.sorted().forEach { print($0, dict[$0] ?? 0) }
